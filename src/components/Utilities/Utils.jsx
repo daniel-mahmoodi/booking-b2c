5fa04f55f396009a1aeb33063a2c70501229cdcc
daniel <mahmoodidanial2@gmail.com>
2024-01-04 14:46:45 +0330

    fix: change status order

--- src/components/Utilities/Utils.jsx
+++ src/components/Utilities/Utils.jsx
@@ -3,7 +3,7 @@ export const clickable = (event) => {
 };
 
 
-export const ticketStatus = ["تایید شده", "کنسل شده", "درخواست کنسل داده شده", "باطل شده"];
+export const ticketStatus = [ "کنسل شده", "درخواست کنسل داده شده", "باطل شده","تایید شده"];
 export const orderStatus = [" درانتظار پرداخت",'پرداخت شده', "لفو سیستمی"];
 export const paymentMethod = ["آنلاین", "کیف پول"];
 export const paymentsType = [
